ac229f09337d06a9f60549445e0f0f7e1be37b0f
Matthiasgdr <spam68130@gmail.com>
2018-12-12 18:45:36 +0100

    :sparkles: Add Parallax before introduction

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,29 @@
 const begin = document.querySelector('.begin')
 const beginButton = begin.querySelector('.begin-button')
-const introductionSentence = document.querySelectorAll('.sentence')
+const introductionSentences = document.querySelectorAll('.sentence')
+const introductionPictures = document.querySelectorAll('.picture')
+
 
 beginButton.addEventListener('click', () => {
     beginButton.classList.add('clicked')
-    const TimeBeforeIntro = setTimeout(intro, 800)
+    setTimeout(parralax, 300)
+    setTimeout(intro, 4200)
 })
+function parralax(){
+    begin.style.display="none"
+    for (let i = 0; i < introductionPictures.length; i++) {
+        const element = introductionPictures[i];
+        element.classList.add('run')
+    }
+}
 
 function intro(){
-    begin.style.display="none"
-    for (let i = 0; i < introductionSentence.length; i++) {
-        const element = introductionSentence[i];
+    for (let i = 0; i < introductionSentences.length; i++) {
+        const element = introductionSentences[i];
         element.classList.add('animation')
     }
 }
+
 
 function loop(){
     requestAnimationFrame(loop)
