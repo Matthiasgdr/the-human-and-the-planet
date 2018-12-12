b9258b48b2f5897f9c37b9caa9ee169c47e06067
Matthiasgdr <spam68130@gmail.com>
2018-12-12 14:27:22 +0100

    :tada: Add Introduction

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
 const begin = document.querySelector('.begin')
 const beginButton = begin.querySelector('.begin-button')
+const introductionSentence = document.querySelectorAll('.sentence')
 
 beginButton.addEventListener('click', () => {
     beginButton.classList.add('clicked')
-    const timeBeforeHiding = setTimeout(() => {begin.style.display="none"}, 1000)
+    const TimeBeforeIntro = setTimeout(intro, 800)
 })
+
+function intro(){
+    begin.style.display="none"
+    for (let i = 0; i < introductionSentence.length; i++) {
+        const element = introductionSentence[i];
+        element.classList.add('animation')
+    }
+}
+
+function loop(){
+    requestAnimationFrame(loop)
+}
+loop()
