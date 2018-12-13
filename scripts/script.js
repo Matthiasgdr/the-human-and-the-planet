9edc8fb29b465b83ccbd87b8f7aae0b181342050
Matthiasgdr <spam68130@gmail.com>
2018-12-13 15:04:37 +0100

    :sparkles: Finished introduction

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,7 @@ beginButton.addEventListener('click', () => {
     setTimeout(parralax, 300)
     setTimeout(intro, 4200)
 })
+
 function parralax(){
     begin.style.display="none"
     for (let i = 0; i < introductionPictures.length; i++) {
@@ -22,8 +23,24 @@ function intro(){
         const element = introductionSentences[i];
         element.classList.add('animation')
     }
+    setTimeout(() => {changeField(2)}, 2000)
+    setTimeout(() => {changeField(3)}, 4500)
+    setTimeout(() => {changeField(4)}, 6000)
+    setTimeout(() => {changeField(5)}, 7000)
 }
-
+function changeField(index){
+   for (let i = 0; i < introductionPictures.length; i++) {
+       const element = introductionPictures[i];
+       element.classList.remove('animation', 'run')
+   }
+    setTimeout(() => {
+        introductionPictures[index].style.transform = 'translateY(300px)'
+    }, 300)
+    setTimeout(() => {
+        introductionPictures[index].style.backgroundImage = `url(images/${index}_2.png)`
+        introductionPictures[index].style.transform = 'translateY(0px)'
+    }, 600)
+}
 
 function loop(){
     requestAnimationFrame(loop)
