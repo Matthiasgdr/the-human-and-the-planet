584d5a24c00fff11d699808f534e45a0aeaa0021
Wizeeps <spam68130@gmail.com>
2018-12-14 17:55:22 +0100

    :sparkles: Add second scene:

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,6 @@ gulp.task('sass', function () {
  
 gulp.task('sass:watch', function () {
   gulp.watch('src/scss/app.scss', ['sass']);
-  gulp.watch('src/scss/_main.scss', ['sass']);
+  gulp.watch('src/scss/_intro.scss', ['sass']);
+  gulp.watch('src/scss/_mission.scss', ['sass']);
 });
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 const begin = document.querySelector('.begin')
+const $container = document.querySelector('.container')
 const beginButton = begin.querySelector('.begin-button')
 const introductionSentences = document.querySelectorAll('.sentence')
 const introductionPictures = document.querySelectorAll('.picture')
 
+// BOUTON COMMENCER
 
 beginButton.addEventListener('click', () => {
     beginButton.classList.add('clicked')
     setTimeout(parralax, 300)
     setTimeout(intro, 4200)
 })
+
+// CACHER BOUTON COMMENCER PUIS AJOUT CLASS RUN POUR L'ANIMATION DU PARALLAX
 
 function parralax(){
     begin.style.display="none"
@@ -17,6 +21,8 @@ function parralax(){
         element.classList.add('run')
     }
 }
+
+// DEFILEMENT DES PHRASES DE L'INTRO ET APPELLE DS CHANGEMENTS DE TERRAINS
 
 function intro(){
     for (let i = 0; i < introductionSentences.length; i++) {
@@ -28,6 +34,9 @@ function intro(){
     setTimeout(() => {changeField(4)}, 10500)
     setTimeout(() => {changeField(5)}, 14000)
 }
+
+// CHANGEMENT DE TERRAIN
+
 function changeField(index){
    for (let i = 0; i < introductionPictures.length; i++) {
        const element = introductionPictures[i];
@@ -40,6 +49,14 @@ function changeField(index){
         introductionPictures[index].style.backgroundImage = `url(images/${index}_2.png)`
         introductionPictures[index].style.transform = 'translateY(0px)'
     }, 600)
+}
+
+
+introductionSentences[4].addEventListener('click', changeScene)
+
+function changeScene(){
+    let scroll = 100
+    $container.style.transform = `translateY(-${scroll}vh)`
 }
 
 function loop(){
